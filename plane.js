acd28bd51bd2a411e56054d454af2532ad5b6e45
Eldridge Gomes <gomes.eldridge@gmail.com>
2020-03-28 17:24:12 +0530

    Simpler example

--- plane.js
+++ plane.js
@@ -1,58 +1,10 @@
-var passengers = [ { name: "Jane Doloop", paid: true, ticket: "coach" },
- { name: "Dr. Evel", paid: true, ticket: "firstclass" },
- { name: "Sue Property", paid: false, ticket: "firstclass" },
- { name: "John Funcall", paid: true, ticket: "coach" } 
-]; 
-
-/* function serveCustomer(passenger) {
-    createDrinkOrder(passenger);
-    // get dinner order
-    createDrinkOrder(passenger);
-    createDrinkOrder(passenger);
-    // show movie
-    createDrinkOrder(passenger);
-    // pick up trash
-} */
-
-function serveCustomer(passenger) {
-    var getDrinkOrderFunction = createDrinkOrder(passenger);
-    getDrinkOrderFunction();
-    // get dinner order
-    getDrinkOrderFunction();
-    getDrinkOrderFunction();
-    // show movie
-    getDrinkOrderFunction();
-    // pick up trash
+function addN(n) {
+    var adder = function(x) {
+        return n + x;
+    };
+    return adder;
 }
 
-
-/* function createDrinkOrder(passenger) {
-    if (passenger.ticket === "firstclass") {
-        alert("Would you like a cocktail or wine?");
-    } else {
-        alert("Your choice is cola or water.");
-    }
-} */
-
-function createDrinkOrder(passenger) {
-    var orderFunction;
-    if (passenger.ticket === "firstclass") {
-        orderFunction = function() {
-            alert("Would you like a cocktail or wine?");
-        };
-    } else {
-        orderFunction = function() {
-            alert("Your choice is cola or water.");
-        };
-    }
-    return orderFunction;
-}
-
-// invocation
-function servePassengers(passengers) {
-    for (var i = 0; i < passengers.length; i++) {
-        serveCustomer(passengers[i]);
-    }
-}
-
-servePassengers(passengers);
+var add2 = addN(2);
+console.log(add2(100)); // 102
+console.log(add2(1000)); // 1002
