d7813504bb1eb33c5334523edb712bae8800150c
Eldridge Gomes <gomes.eldridge@gmail.com>
2020-03-29 13:30:09 +0530

    Multilevel inheritance

--- prototype.js
+++ prototype.js
@@ -5,69 +5,78 @@ function Dog(name, breed, weight) {
 }
 
 Dog.prototype.species = "Canine";
-
+Dog.prototype.sitting = false;
+Dog.prototype.sit = function() {
+	if (this.sitting) {
+		console.log(this.name + " is already sitting");
+	} else {
+		console.log(this.name + " is now sitting");
+		this.sitting = true;
+	}
+}
 Dog.prototype.bark = function() {
-    if (this.weight > 25) {
-        console.log(this.name + " says Woof!");
-    } else {
-        console.log(this.name + " says Yip!");
-    }
+	if (this.weight > 25) {
+		console.log(this.name + " says Woof!");
+	} else {
+		console.log(this.name + " says Yip!");
+	}
 };
-
 Dog.prototype.run = function() {
     console.log("Run!");
 };
-
 Dog.prototype.wag = function() {
     console.log("Wag!");
 };
 
-var fido = new Dog("Fido", "Mixed", 38);
-var fluffy = new Dog("Fluffy", "Poodle", 30);
-var spot = new Dog("Spot", "Chihuahua", 10);
-
-fido.bark();
-fido.run();
-fido.wag();
-
-fluffy.bark();
-fluffy.run();
-fluffy.wag();
-
-spot.bark();
-spot.run();
-spot.wag();
-
-spot.bark = function() {
-    console.log(this.name + " says WOOF!");
-};
-
-spot.bark();
-spot.run();
-spot.wag();
-
-var barnaby = new Dog("Barnaby", "Basset Hound", 55);
-
-Dog.prototype.sitting = false;
-
-Dog.prototype.sit = function() {
-    if (this.sitting) {
-        console.log(this.name + " is already sitting");
-    } else {
-        this.sitting = true;
-        console.log(this.name + " is now sitting");
-    }
+function ShowDog(name, breed, weight, handler) {
+	this.name = name;
+	this.breed = breed;
+	this.weight = weight;
+	this.handler = handler;
 }
 
-barnaby.sit();
-barnaby.sit();
-fluffy.sit();
-fluffy.sit();
+ShowDog.prototype = new Dog(); 
 
-spot.hasOwnProperty("species"); //false since its in prototype and not instance
-fido.hasOwnProperty("species"); //false
+ShowDog.prototype.league = "Webville";
 
-console.log(spot.hasOwnProperty("sitting"));
-spot.sitting = true;
-console.log(spot.hasOwnProperty("sitting")); //true since iits taking from instance
-console.log(fido.hasOwnProperty("sitting")); //false since its taking from prototyoe and not instance
+ShowDog.prototype.stack = function() { 
+	console.log("Stack");
+};
+
+ShowDog.prototype.bait = function() {
+	console.log("Bait");
+};
+
+ShowDog.prototype.gait = function(kind) {
+	console.log(kind + "ing");
+};
+
+ShowDog.prototype.groom = function() {
+	console.log("Groom");
+};
+
+/*var scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
+var beatrice = new ShowDog("Beatrice", "Pomeranian", 5, "Hamilton");
+scotty.stack();
+scotty.bark();
+console.log(scotty.league);
+console.log(scotty.species);*/
+
+var fido = new Dog("Fido", "Mixed", 38);
+if (fido instanceof Dog) {
+    console.log("Fido is a Dog");
+}
+if (fido instanceof ShowDog) {
+    console.log("Fido is a ShowDog");
+}
+
+var scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
+if (scotty instanceof Dog) {
+    console.log("Scotty is a Dog");
+}
+if (scotty instanceof ShowDog) {
+    console.log("Scotty is a ShowDog");
+}
+
+console.log("Fido constructor is " + fido.constructor);
+console.log("Scotty constructor is " + scotty.constructor);
