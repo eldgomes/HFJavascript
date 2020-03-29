0f4cce0904087bc751b822b9470883ea12615326
Eldridge Gomes <gomes.eldridge@gmail.com>
2020-03-29 13:48:23 +0530

    Override

--- prototype.js
+++ prototype.js
@@ -1,95 +1,15 @@
-function Dog(name, breed, weight) {
+function Robot(name, year, owner) {
     this.name = name;
-    this.breed = breed;
-    this.weight = weight;
+    this.year = year;
+    this.owner = owner;
 }
 
-Dog.prototype.species = "Canine";
-Dog.prototype.sitting = false;
-Dog.prototype.sit = function() {
-	if (this.sitting) {
-		console.log(this.name + " is already sitting");
-	} else {
-		console.log(this.name + " is now sitting");
-		this.sitting = true;
-	}
-}
-Dog.prototype.bark = function() {
-	if (this.weight > 25) {
-		console.log(this.name + " says Woof!");
-	} else {
-		console.log(this.name + " says Yip!");
-	}
-};
-Dog.prototype.run = function() {
-    console.log("Run!");
-};
-Dog.prototype.wag = function() {
-    console.log("Wag!");
-};
+var toy1 = new Robot("Toy", 2013, "Avary");
+console.log(toy1.toString()); // returns [object Object]
 
-function ShowDog(name, breed, weight, handler) {
-	Dog.call(this, name, breed, weight); //replaced redundant code
-	this.handler = handler;
-}
-
-ShowDog.prototype = new Dog(); 
-ShowDog.prototype.constructor = ShowDog;
-
-ShowDog.prototype.league = "Webville";
-
-ShowDog.prototype.stack = function() { 
-	console.log("Stack");
-};
-
-ShowDog.prototype.bait = function() {
-	console.log("Bait");
-};
-
-ShowDog.prototype.gait = function(kind) {
-	console.log(kind + "ing");
-};
-
-ShowDog.prototype.groom = function() {
-	console.log("Groom");
-};
-
-/*var scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
-var beatrice = new ShowDog("Beatrice", "Pomeranian", 5, "Hamilton");
-scotty.stack();
-scotty.bark();
-console.log(scotty.league);
-console.log(scotty.species);*/
-
-var fido = new Dog("Fido", "Mixed", 38);
-if (fido instanceof Dog) {
-    console.log("Fido is a Dog");
-}
-if (fido instanceof ShowDog) {
-    console.log("Fido is a ShowDog");
-}
-
-var scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
-if (scotty instanceof Dog) {
-    console.log("Scotty is a Dog");
-}
-if (scotty instanceof ShowDog) {
-    console.log("Scotty is a ShowDog");
-}
-
-console.log("Fido constructor is " + fido.constructor);
-console.log("Scotty constructor is " + scotty.constructor); //shows Dog constructor without line 37
-
-/*****************/
-var fido = new Dog("Fido", "Mixed", 38);
-var fluffy = new Dog("Fluffy", "Poodle", 30);
-var spot = new Dog("Spot", "Chihuahua", 10);
-var scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
-var beatrice = new ShowDog("Beatrice", "Pomeranian", 5, "Hamilton");
-fido.bark();
-fluffy.bark();
-spot.bark();
-scotty.bark();
-beatrice.bark();
-scotty.gait("Walk");
-beatrice.groom();
+Robot.prototype.toString = function() {
+    return this.name + " Robot belonging to " + this.owner;
+   };
+var toy2 = new Robot("Toy", 2013, "Avary");
+console.log(toy2.toString());
+console.log("Robot is: " + toy2); //toy2 is converted to string
